4272b43b864e8004a7509bceaebbe469e1655d05
Emilien Marquegnies <marquegniesemilien@gmail.com>
2024-10-05 16:42:48 +0200

    Git removed api key (regenerated it) and fix port issue

--- src/app/controllers/movies.ts
+++ src/app/controllers/movies.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+import 'dotenv/config'
+
 const options = {
     method: 'GET',
     url: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTZiMzliYzUzYmQwOWNiZmY5Zjk4NzA4YmEwMTU4NSIsIm5iZiI6MTcyODA1MTA4Ni4yMDQ1NDgsInN1YiI6IjY2MGU5ZTg4ZTE4ZTNmMDE3ZGE0MTgzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Qsa89J9nO-5uYigZFKrvlN9lDrkctply5rpiyqLrZZ0'
+      Authorization: `Bearer ${process.env.TMDB_TOKEN}` 
     }
   };
 
--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,10 @@ import Connection from './app/services/orm/connection.js'
 
 
 
-const port = process.env.PORT || '3500'
+const port = process.env.SERVER_PORT || '3500'
 
-app.listen(process.env.PORT, () => {
-    console.log("Server started");
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 
     const con = Connection.instance;
 })
