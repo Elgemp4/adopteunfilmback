9666205868cb0342a31c10ef3e73d09decae360f
Emilien Marquegnies <marquegniesemilien@gmail.com>
2024-10-11 18:32:57 +0200

    Advances

--- src/app/controllers/providersController.ts
+++ src/app/controllers/providersController.ts
@@ -1,11 +1,16 @@
 import { matchedData, validationResult } from "express-validator";
 import { getProviders } from "../services/fetcher/providersFetcher.js";
 import { RequestHandler } from "express";
+import { saveIfNoExistsProvider } from "../services/orm/providerORM.js";
 
 export const getAllProviders :RequestHandler = async (req, res) => {
     const {region, language} = matchedData(req);
 
     const result = await getProviders(language, region);
+
+    saveIfNoExistsProvider(result);
+
+    
     res.json(result);
 }
 
--- src/app/services/orm/providerORM.ts
+++ src/app/services/orm/providerORM.ts
@@ -1,9 +1,13 @@
-export async function saveIfNoExistsProvider() {
+/**
+ * Add providers in the repository if they aren't already added
+ */
+export async function saveIfNoExistsProvider(providers) {
 
 }
 
+/**
+ * Save the user's providers (and override previous providers)
+ */
 export async function saveUserProviders() {
 
 }
-
-export async function 
