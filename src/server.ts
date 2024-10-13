9d0c23cf67df4ad1adcc15161ebe961696e9ab05
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2024-10-13 18:59:38 +0200

    Added genre fetching at server startup

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,20 @@ import app from './app/index.js'
 import 'dotenv/config'
 
 import AppDataSource from './app/data-source.js'
+import { getGenre, saveGenreIfNotExist } from './app/services/orm/genreORM.js'
+import { getGenres } from './app/services/fetcher/genresFetcher.js'
 
 const port = process.env.SERVER_PORT || '3500'
 
 AppDataSource.initialize()
-    .then(() => {
+    .then(async () => {
         console.log("Connected to database");
+
+        const genres = await getGenres();
+
+        for(const genre of genres){
+            saveGenreIfNotExist(genre.id, genre.name);
+        }
     })
     .catch((error) => console.log(error))
 
