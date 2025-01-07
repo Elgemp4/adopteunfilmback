d5ab93fbfb181f354474e52d4ab1e3065dd4069d
DrKreijger <sebdek7@gmail.com>
2025-01-07 15:46:04 +0100

    Fixed seen 2
    J'ai oublié de changer le parse xd

--- src/app/services/store/movieStore.ts
+++ src/app/services/store/movieStore.ts
@@ -104,7 +104,7 @@ export async function evaluateMovie(userId, movieId, appreciate, seen){
     }
     
     review.appreciate = JSON.parse(appreciate);
-    review.seen = JSON.parse(appreciate);
+    review.seen = JSON.parse(seen);
     movieReviewRepo.save(review);
 }
 
